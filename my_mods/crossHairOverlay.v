0af92ad3262deddb83073c04f66c546e48786ac8
Johann Lam <lamjohann549@gmail.com>
2025-07-26 15:29:55 -0700

    Update crossHairOverlay.v

--- my_mods/crossHairOverlay.v
+++ my_mods/crossHairOverlay.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Johann Lam
 // 
 // Create Date: 07/11/2025 11:28:19 PM
 // Design Name: 
@@ -19,65 +19,125 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+// --------------------------------------------------------------------------
+// This module is an fsm that essentially latches on whether the red object valid
+// is asserted, so if the upstream centroid calculation module determines the 
+// red object is valid, it will display the regular pixel data but change 
+// the centroid pixels to green, if the centroid module doesnt detect a valid object
+// the entire frame is fed unchanged 
+//---------------------------------------------------------------------------
 
 module crossHairOverlay#(
-    parameter crosshair_size = 8
+    parameter crosshair_size = 8,
+    parameter IMG_WIDTH = 640,
+    parameter IMG_HEIGHT = 480
 )
 (
     input wire i_clk,
     input wire i_rstn,
+    
     input wire i_data_valid,
     input wire [11:0] i_data,
-    input wire [9:0] centroid_x,
-    input wire [8:0] centroid_y,
-    input wire end_frame,
+    
+    input wire [9:0] i_centroid_x,
+    input wire [8:0] i_centroid_y,
+    input wire i_end_frame,
+    input wire i_red_object_valid,
     
     output reg o_data_valid,
-    output reg o_data
+    output reg [11:0] o_data
     );
     
-    wire draw_cross;
-    reg [9:0] x_counter;
-    reg [8:0] y_counter;
+    // state encoding
+    reg [1:0] STATE, NEXT_STATE; 
+    localparam STATE_IDLE =  2'b00,
+              STATE_RED =    2'b01,
+              STATE_NO_RED = 2'b10;
     
-assign draw_cross = 
-    ( (x_counter >= centroid_x ? (x_counter - centroid_x) : (centroid_x - x_counter)) <= crosshair_size ) &&
-    ( (y_counter >= centroid_y ? (y_counter - centroid_y) : (centroid_y - y_counter)) <= crosshair_size );
+    wire draw_box;
+    reg [9:0] x_counter, next_x_counter;
+    reg [8:0] y_counter, next_y_counter;
     
-    always @(posedge i_clk) begin
-        if (~i_rstn) begin
-            o_data <= 12'b0;
-            o_data_valid <= 0;
-        end 
-        else if (i_data_valid)begin
-            if(draw_cross) begin
-                o_data <= 12'h0F0; // green crosshair
-                o_data_valid <= i_data_valid;
-            end 
-            else begin
-                o_data <= i_data;
-                o_data_valid <= i_data_valid;
+    reg [11:0] next_data;
+    reg next_o_data_valid;
+    
+    assign draw_box = 
+    ( (x_counter >= i_centroid_x ? (x_counter - i_centroid_x) : (i_centroid_x - x_counter)) <= crosshair_size ) ||
+    ( (y_counter >= i_centroid_y ? (y_counter - i_centroid_y) : (i_centroid_y - y_counter)) <= crosshair_size );
+    
+    
+    always@*begin
+        next_data = i_data;
+        NEXT_STATE = STATE;
+        next_x_counter    = x_counter;
+        next_y_counter    = y_counter;
+        next_o_data_valid = 0;
+        case(STATE) 
+        
+            STATE_IDLE: begin
+                if(i_red_object_valid) begin
+                    NEXT_STATE = STATE_RED;
+                end
+                else begin
+                    NEXT_STATE = STATE_NO_RED;
+                end
+                next_x_counter = 0;
+                next_y_counter = 0;
             end
-        end
-        else begin
-            o_data <= 0;
-            o_data_valid <= 0;
-        end
+            
+            STATE_RED: begin
+                if(i_end_frame) begin
+                    NEXT_STATE = STATE_IDLE;
+                    next_x_counter = 0;
+                    next_y_counter = 0;
+                end
+                else if(i_data_valid) begin
+                    next_data = draw_box ? 12'h0F0 : i_data;
+                    next_x_counter = (x_counter == IMG_WIDTH - 1) ? 0 : x_counter + 1;
+                    next_y_counter = (x_counter == IMG_WIDTH - 1) ? y_counter + 1 : y_counter;
+                    next_o_data_valid = 1'b1;
+                end
+            end
+
+            
+            STATE_NO_RED: begin
+                if(i_end_frame) begin
+                    NEXT_STATE = STATE_IDLE;
+                    next_x_counter = 0;
+                    next_y_counter = 0;
+                end
+                else if(i_data_valid) begin
+                    next_data = i_data;
+                    next_x_counter = (x_counter == IMG_WIDTH - 1) ? 0 : x_counter + 1;
+                    next_y_counter = (x_counter == IMG_WIDTH - 1) ? y_counter + 1 : y_counter;
+                    next_o_data_valid = 1'b1;
+                end
+            end
+            
+            default: begin
+                NEXT_STATE = STATE_IDLE;
+                next_x_counter = 0;
+                next_y_counter = 0;
+                next_o_data_valid = 0;
+            end
+        endcase
     end
     
     always@(posedge i_clk)begin
-        if(~i_rstn)begin
+        if (~i_rstn)begin
+            STATE <= STATE_IDLE;
             x_counter <= 0;
             y_counter <= 0;
+            o_data <= 0;
+            o_data_valid <= 0;
         end
-        else if (i_data_valid) begin
-            if (end_frame)begin
-                x_counter <= 0;
-                y_counter <= 0;
-            end
-            x_counter <= (x_counter == 639) ? 0: x_counter + 1'b1;
-            y_counter <= (x_counter == 639) ? y_counter + 1'b1 : y_counter;
+        else begin
+            STATE <= NEXT_STATE;
+            x_counter <= next_x_counter;
+            y_counter <= next_y_counter;
+            o_data <= next_data;
+            o_data_valid <= next_o_data_valid;
+            
         end
     end
-    
 endmodule
