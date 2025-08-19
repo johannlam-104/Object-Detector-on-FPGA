09ad26c4bb55db2728810188332d133607f833dc
Johann Lam <lamjohann549@gmail.com>
2025-08-18 20:19:51 -0700

    Update centroidCalc.v

--- my_mods/centroidCalc.v
+++ my_mods/centroidCalc.v
@@ -23,8 +23,7 @@
 module centroidCalc #(
     parameter IMG_WIDTH = 640,
     parameter IMG_HEIGHT = 480,
-    parameter PIXEL_THRESHOLD = 1000,
-    parameter DENSITY_THRESHOLD = 40  // in percent, no decimal
+    parameter PIXEL_THRESHOLD = 1000 // minimum amount of red pixels to be considered valid object
 )(
     input  wire        i_clk,
     input  wire        i_rstn,
@@ -32,117 +31,117 @@ module centroidCalc #(
     input  wire        i_valid_red_pixel,
     input  wire        i_valid,
 
-    output reg  [9:0]  centroid_x,
-    output reg  [8:0]  centroid_y,
-    output reg         valid,
-    output reg         red_object_valid,
-    output reg         end_frame
+    output reg  [9:0]  o_centroid_x,
+    output reg  [8:0]  o_centroid_y,
+    output reg         o_valid,
+    output reg         o_red_object_valid,
+    
+    output reg         o_end_frame
+    
 );
 
-    // Compute required bit widths
     localparam X_WIDTH = $clog2(IMG_WIDTH);
     localparam Y_WIDTH = $clog2(IMG_HEIGHT);
 
-    // Position trackers
     reg [X_WIDTH-1:0] x_counter;
     reg [Y_WIDTH-1:0] y_counter;
 
-    // Bounding box and centroid accumulation
     reg [X_WIDTH-1:0] closest_x, furthest_x;
     reg [Y_WIDTH-1:0] closest_y, furthest_y;
-    reg [18:0]        red_pixel_counter;
-    reg [31:0]        sum_x, sum_y;
-    reg               end_frame_d;
+    reg [18:0]   red_pixel_counter;
 
-    // Main position tracker
+    wire         red_object_qualifies;
+
+    wire at_last_pixel = (x_counter == IMG_WIDTH - 1) && (y_counter == IMG_HEIGHT - 1);
+    
+    // end frame logic
+    reg end_frame;
+    reg end_frame_d;
+    reg end_frame_sync;
+   
+
+    // counters
     always @(posedge i_clk) begin
-        if (~i_rstn) begin
+        if (~i_rstn || end_frame) begin
             x_counter <= 0;
             y_counter <= 0;
-            end_frame <= 0;
-        end else if (i_valid) begin
-            if (x_counter == IMG_WIDTH - 1) begin
-                x_counter <= 0;
-                if (y_counter == IMG_HEIGHT - 1) begin
-                    y_counter <= 0;
-                    end_frame <= 1;
-                end else begin
-                    y_counter <= y_counter + 1;
-                    end_frame <= 0;
-                end
-            end else begin
-                x_counter <= x_counter + 1;
-                end_frame <= 0;
-            end
-        end else begin
-            end_frame <= 0;
+        end 
+        else if (i_valid) begin
+            x_counter <= (x_counter == IMG_WIDTH-1) ? 0 : x_counter + 1'b1;
+            y_counter <= (x_counter == IMG_WIDTH-1) ? y_counter + 1'b1 : y_counter;
         end
     end
-    
-    always@(posedge i_clk)begin
-        if(~i_rstn)
-            end_frame_d <= 0;
-        else
+
+    // End-frame flag
+    always @(posedge i_clk) begin
+        if (~i_rstn)
+            o_end_frame <= 0;
+        else begin
             end_frame_d <= end_frame;
+            end_frame_sync <= (at_last_pixel & i_valid);
+            o_end_frame <= end_frame_sync;
+        end
     end
 
-    // Accumulate red pixel stats
+    // Red pixel accumulation
     always @(posedge i_clk) begin
         if (~i_rstn || end_frame_d) begin
             red_pixel_counter <= 0;
-            sum_x <= 0;
-            sum_y <= 0;
             closest_x <= IMG_WIDTH - 1;
             closest_y <= IMG_HEIGHT - 1;
             furthest_x <= 0;
             furthest_y <= 0;
-        end else if (i_valid && i_valid_red_pixel) begin
+        end 
+        else if (i_valid && i_valid_red_pixel) begin
             red_pixel_counter <= red_pixel_counter + 1;
-            sum_x <= sum_x + x_counter;
-            sum_y <= sum_y + y_counter;
-
-            // Closest red pixel to top-left
-            if (y_counter < closest_y || (y_counter == closest_y && x_counter < closest_x)) begin
+            if (y_counter < closest_y || (y_counter == closest_y && x_counter < closest_x)) begin // upper left most red pixel
                 closest_x <= x_counter;
                 closest_y <= y_counter;
             end
-
-            // Furthest red pixel from top-left
-            if (y_counter > furthest_y || (y_counter == furthest_y && x_counter > furthest_x)) begin
+            if (y_counter > furthest_y || (y_counter == furthest_y && x_counter > furthest_x)) begin // lowest right most pixel
                 furthest_x <= x_counter;
                 furthest_y <= y_counter;
             end
         end
     end
-
-    // Final density and centroid calculation
-    wire [X_WIDTH:0] box_width  = furthest_x - closest_x + 1;
-    wire [Y_WIDTH:0] box_height = furthest_y - closest_y + 1;
-    wire [31:0] area = box_width * box_height;
-
-    wire [31:0] scaled_red  = red_pixel_counter * 100;
-    wire [31:0] scaled_area = area * DENSITY_THRESHOLD;
-
+    
+    assign red_object_qualifies = (red_pixel_counter >= PIXEL_THRESHOLD); // if my object is big enough
+    
+    // Output division results
     always @(posedge i_clk) begin
-        if (~i_rstn) begin
-            red_object_valid <= 0;
-            centroid_x <= 0;
-            centroid_y <= 0;
-            valid <= 0;
-        end else if (end_frame) begin
-            red_object_valid <= ((scaled_red >= scaled_area) && red_pixel_counter >= PIXEL_THRESHOLD);
-            if (red_pixel_counter != 0) begin
-                centroid_x <= sum_x / red_pixel_counter;
-                centroid_y <= sum_y / red_pixel_counter;
-                valid <= 1;
-            end else begin
-                centroid_x <= 0;
-                centroid_y <= 0;
-                valid <= 0;
-            end
-        end else begin
-            valid <= 0;
+        if (~i_rstn || end_frame_d) begin
+            o_centroid_x <= 0;
+            o_centroid_y <= 0;
+            o_valid <= 0;
+            o_red_object_valid <= 0;
+            end_frame <= 0;
+        end 
+        else if (at_last_pixel && red_object_qualifies && i_valid) begin
+            o_centroid_x <= (furthest_x + closest_x) >> 1; // add and bitshift in 1 cycle
+            o_centroid_y <= (furthest_y + closest_y) >> 1;
+            o_valid <= 1;
+            o_red_object_valid <= 1;
+            end_frame <= 1;
+        end 
+        else if (i_valid && at_last_pixel) begin
+            o_centroid_x <= 0;
+            o_centroid_y <= 0;
+            o_valid <= 1;
+            o_red_object_valid <= 0;
+            end_frame <= 1;
+        end
+        else if(i_valid)begin
+            o_centroid_x <= 0;
+            o_centroid_y <= 0;
+            o_valid <= 1;
+            o_red_object_valid <= 0;
+            end_frame <= 0;
+        end
+        else begin
+            end_frame <= 0;
+            o_valid <= 0;
+            o_red_object_valid <= 0;
         end
     end
-
 endmodule
+
